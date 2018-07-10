088db7b7fa5436b529fadd235ae4a2e08869622d
WestSIDE <eastside@bigmir.net>
2018-07-11 00:53:11 +0300

    Add simple user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports.login = (function (req, res) {
@@ -9,11 +10,27 @@ module.exports.login = (function (req, res) {
     });
 });
 
-module.exports.register = (function (req, res) {
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password,
-    });
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email});
 
-    user.save().then(() => console.log('User was successfully created'))
-});
+    if (candidate) {
+        res.status(409).json({
+            message: 'Такой email уже занят. Попробуйте другой',
+        });
+    } else {
+
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt),
+        });
+
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (e) {
+            console.log(e); // Обработать ошибку
+        }
+    }
+};
