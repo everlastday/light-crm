287afb0aa9f4dcde5846c099cadc1715edb768f3
WestSIDE <eastside@bigmir.net>
2018-07-17 00:09:06 +0300

    Add user login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://user:a123456@ds129541.mlab.com:29541/fullstack'
+    mongoURI: 'mongodb://user:a123456@ds129541.mlab.com:29541/fullstack',
+    jwt: 'dev-jswt'
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,37 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jsonWebToken = require('jsonwebtoken');
+const keys = require('../config/keys');
 
-module.exports.login = (function (req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password,
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            // We need to generate token
+
+            const token = jsonWebToken.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+            res.status(200).json({
+                token: `Bearer ${token}`
+            });
+        } else {
+            res.status(401).json({
+                message: 'Пароли не совпадают. Попробуйте снова.'
+            });
         }
-    });
-});
+        // Check user password
+    } else {
+        // No user, error
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден.'
+        })
+    }
+
+};
 
 module.exports.register = async function (req, res) {
     const candidate = await User.findOne({email: req.body.email});
