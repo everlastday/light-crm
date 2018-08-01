3ffb470f8d2982bc5f02e3dc36a3f3b70d1b0900
WestSIDE <eastside@bigmir.net>
2018-08-01 18:41:50 +0300

    Create and update categories

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
 
 app.use(passport.initialize());
 require('./middleware/passport')(passport);
-
+app.use('/uploads', express.static('uploads'));
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(require('morgan')('dev'));
 app.use(require('cors')());
 
 app.use('/api/auth', authRoutes);
--- middleware/upload.js
+++ middleware/upload.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
     },
     filename(req, file, callback) {
         const date = moment().format('DDMMYYYY-HHmmss_SSS');
-        callback(null, '{date}-${file.originalname}')
+        callback(null, `${date}-${file.originalname}`)
     },
 });
 const fileFilter = (req, file, callback) => {
