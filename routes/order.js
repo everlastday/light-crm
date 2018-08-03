5303c1fcec2b66839425bf7b06be3ad379af7c33
WestSIDE <eastside@bigmir.net>
2018-08-04 01:56:38 +0300

    Add order functionality

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ const express = require('express');
 const controller = require('../controllers/order');
 const router = express.Router();
 
-router.get('/', controller.getByAll);
-router.post('/', controller.create);
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getByAll);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create);
 
 module.exports = router;
