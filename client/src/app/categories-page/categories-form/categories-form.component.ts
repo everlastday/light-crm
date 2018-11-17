bc04b5dc06a514ac4cbd32561379294f71bfb140
WestSIDE <eastside@bigmir.net>
2018-11-17 15:14:13 +0200

    Image upload functionality

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {CategoriesService} from "../../shared/services/categories.service";
-import {switchMap} from "rxjs/operators";
-import {of} from "rxjs/index";
-import {MaterialService} from "../../shared/classes/material.service";
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core'
+import {ActivatedRoute, Params} from '@angular/router'
+import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {CategoriesService} from '../../shared/services/categories.service'
+import {switchMap} from 'rxjs/operators'
+import {of} from 'rxjs'
+import {MaterialService} from '../../shared/classes/material.service'
+import {Category} from '../../shared/interfaces'
 
 @Component({
     selector: 'app-categories-form',
@@ -13,10 +14,15 @@ import {MaterialService} from "../../shared/classes/material.service";
 })
 export class CategoriesFormComponent implements OnInit {
 
+    @ViewChild('input') inputRef: ElementRef;
     form: FormGroup;
+    image: File;
+    imagePreview = '';
     isNew = true;
+    category: Category;
 
-    constructor(private route: ActivatedRoute, private categoriesService: CategoriesService) {
+    constructor(private route: ActivatedRoute,
+                private categoriesService: CategoriesService) {
     }
 
     ngOnInit() {
@@ -26,31 +32,74 @@ export class CategoriesFormComponent implements OnInit {
 
         this.form.disable();
 
-        this.route.params.pipe(
-            switchMap(
-                (params: Params) => {
-                    if (params['id']) {
-                        this.isNew = false;
-                        return this.categoriesService.getById(params['id']);
+        this.route.params
+            .pipe(
+                switchMap(
+                    (params: Params) => {
+                        if (params['id']) {
+                            this.isNew = false
+                            return this.categoriesService.getById(params['id'])
+                        }
+
+                        return of(null)
+                    }
+                )
+            )
+            .subscribe(
+                (category: Category) => {
+                    if (category) {
+                        this.category = category
+                        this.form.patchValue({
+                            name: category.name
+                        });
+                        this.imagePreview = category.imageSrc;
+                        MaterialService.updateTextInputs()
                     }
 
-                    return of(null)
-                }
+                    this.form.enable()
+                },
+                error => MaterialService.toast(error.error.message)
             )
-        ).subscribe(category => {
-            if (category) {
-                this.form.patchValue({
-                    name: category.name
-                });
-                MaterialService.updateTextInputs();
-            }
-            this.form.enable();
-        }, error => MaterialService.toast(error.error.meaning));
+    }
 
+    triggerClick() {
+        this.inputRef.nativeElement.click();
+    }
+
+    onFileUpload(event: any) {
+        const file = event.target.files[0];
+        this.image = file;
+
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            this.imagePreview = reader.result;
+        };
+
+        reader.readAsDataURL(file)
     }
 
     onSubmit() {
+        let obs$
+        this.form.disable();
 
+        if (this.isNew) {
+            obs$ = this.categoriesService.create(this.form.value.name, this.image)
+        } else {
+            obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image)
+        }
+
+        obs$.subscribe(
+            category => {
+                this.category = category
+                MaterialService.toast('Изменения сохранены.');
+                this.form.enable()
+            },
+            error => {
+                MaterialService.toast(error.error.message);
+                this.form.enable()
+            }
+        )
     }
 
 }
--- controllers/category.js
+++ controllers/category.js
@@ -55,9 +55,8 @@ module.exports.update = async function (req, res) {
         updated.imageSrc = req.file.path
     }
     try {
-
         const category = await Category.findOneAndUpdate(
-            {id: req.params.id},
+            {_id: req.params.id},
             {$set: updated},
             {new: true}
         );
